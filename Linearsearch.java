be633075ce07a42ca381f7014da8504798236fb7
Hardik-Kakuste <hp799078@gmail.com>
2026-06-06 14:26:44 +0530

    Add files via upload

---
+++ Linearsearch.java
@@ -0,0 +1,14 @@
+public class Linearsearch{
+    public static void main(string[]args){
+        int[] arr = {2,5,3,6,4};
+        int target = 3;
+        boolean found =  false;
+        for (int i = 0; i < arr.length; i++) {
+            if arr[i] ==  target {
+                found = true;
+                break;
+            }
+        }
+
+    }
+}
---
+++ PrintNumbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class PrintNumbers {
+    
+    static void print(int n) {
+        if (n == 0) {
+            return;
+        }
+
+        print(n - 1);
+        System.out.print(n + " ");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        print(n);
+    }
+}
